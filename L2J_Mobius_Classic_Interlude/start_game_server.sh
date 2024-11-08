0d93bfb104bcf00474b097812a3eb84c4b1e3b50
Michael Melo <e_michel.melo@metrosp.com.br>
2024-11-08 13:29:41 -0300

    ajuste docker compose e file

--- L2J_Mobius_Classic_Interlude/start_game_server.sh
+++ L2J_Mobius_Classic_Interlude/start_game_server.sh
@@ -8,16 +8,20 @@ ls -la /usr/bin/cp
 ldd /usr/bin/cp
 
 # Criar diretórios necessários
+echo "Criando diretórios /host-dist/data/libs e /host-dist/data/game/log"
 mkdir -p /host-dist/data/libs
 mkdir -p /host-dist/data/game/log
 
 # Verificar se /host-dist/data/libs foi criado
+echo "Verificando diretórios criados em /host-dist/data"
 ls -la /host-dist/data
 
 # Copiar o arquivo JAR para o diretório de destino
+echo "Copiando GameServer.jar para /host-dist/data/libs/"
 cp /opt/l2/GameServer.jar /host-dist/data/libs/GameServer.jar
 
 # Iniciar a tarefa do Game Server
+echo "Navegando para /scripts/ e executando GameServerTask.sh"
 cd /scripts/
 ls -la
 ./GameServerTask.sh
--- L2J_Mobius_Classic_Interlude/start_login_server.sh
+++ L2J_Mobius_Classic_Interlude/start_login_server.sh
@@ -8,16 +8,20 @@ ls -la /usr/bin/cp
 ldd /usr/bin/cp
 
 # Criar diretórios necessários
+echo "Criando diretórios /host-dist/data/libs e /host-dist/data/login/log"
 mkdir -p /host-dist/data/libs
 mkdir -p /host-dist/data/login/log
 
 # Verificar se /host-dist/data/libs foi criado
+echo "Verificando diretórios criados em /host-dist/data"
 ls -la /host-dist/data
 
 # Copiar o arquivo JAR para o diretório de destino
+echo "Copiando LoginServer.jar para /host-dist/data/libs/"
 cp /opt/l2/LoginServer.jar /host-dist/data/libs/LoginServer.jar
 
 # Iniciar a tarefa do Login Server
+echo "Navegando para /scripts/ e executando LoginServerTask.sh"
 cd /scripts/
 ls -la
 ./LoginServerTask.sh
